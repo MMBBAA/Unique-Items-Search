ee0039e9eaaa802ca87d181f328c169e75bc7f36
Alberto_BM <a-bedmarmto@outlook.com>
2021-03-05 19:41:52 +0100

    Update uniqueItems.js

--- uniqueItems.js
+++ uniqueItems.js
@@ -1,27 +1,26 @@
 	window.onload=function(){
-			document.getElementById("clear").addEventListener("click", clear);
-			document.getElementById("user").addEventListener("keyup", test);	
+		document.getElementById("clear").addEventListener("click", clear);
+		document.getElementById("user").addEventListener("keyup", test);	
 			
 	}
 
 	function test(){
 			
-		var	word=document.getElementById("user").value;
+		var word=document.getElementById("user").value;
 				duplicateEncode(word);
 	}
 
 	function clear(){
-		console.log("entra");
 		document.getElementById("rdo").innerHTML="";
-        document.getElementById("user").innerHTML="";
+        	document.getElementById("user").innerHTML="";
 	}
 	
 	function duplicateEncode(word){
 	
-	let empty=[];//array with word repeated without filtering
-	let rdo;//final answer
-	let compare=word.toLowerCase().split("");//word converted to array in Lowercase	
-	let repeated=[];//words repeated
+		let empty=[];//array with word repeated without filtering
+		let rdo;//final answer
+		let compare=word.toLowerCase().split("");//word converted to array in Lowercase	
+		let repeated=[];//words repeated
  
  		for(var i=0;i<compare.length;i++){
  			for(var j=0;j<compare.length;j++){
