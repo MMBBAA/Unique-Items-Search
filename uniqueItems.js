bf59db4cbea2638559867447d9422d7a070e56eb
Alberto_BM <a-bedmarmto@outlook.com>
2021-03-05 19:38:14 +0100

    Add files via upload

---
+++ uniqueItems.js
@@ -0,0 +1,37 @@
+	window.onload=function(){
+			document.getElementById("clear").addEventListener("click", clear);
+			document.getElementById("user").addEventListener("keyup", test);	
+			
+	}
+
+	function test(){
+			
+		var	word=document.getElementById("user").value;
+				duplicateEncode(word);
+	}
+
+	function clear(){
+		console.log("entra");
+		document.getElementById("rdo").innerHTML="";
+        document.getElementById("user").innerHTML="";
+	}
+	
+	function duplicateEncode(word){
+	
+	let empty=[];//array with word repeated without filtering
+	let rdo;//final answer
+	let compare=word.toLowerCase().split("");//word converted to array in Lowercase	
+	let repeated=[];//words repeated
+ 
+ 		for(var i=0;i<compare.length;i++){
+ 			for(var j=0;j<compare.length;j++){
+ 				if(compare[i]==compare[j]&&i!=j){
+ 					empty.push(compare[i]);	
+ 				}
+ 			}
+ 		}
+ 	
+ 		rdo=repeated=[...new Set(empty)].toString().replace(/,/g," ");
+
+ 		document.getElementById("rdo").innerHTML=`Items repeated: ${rdo}`;
+	}
